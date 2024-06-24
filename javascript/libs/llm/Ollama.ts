fd74a4322a7c716babe4fd81f71210b001cf9c21
dailinzhou <zhoudailin.cser@outlook.com>
2024-06-25 01:30:25 +0800

    RAG

--- javascript/libs/llm/Ollama.ts
+++ javascript/libs/llm/Ollama.ts
@@ -2,7 +2,7 @@ import {ChatOllama} from '@langchain/community/chat_models/ollama'
 
 const llm = new ChatOllama({
     baseUrl: "http://localhost:11434",
-    model: "qwen:14b",
+    model: "qwen:7b",
 })
 
 export default llm
